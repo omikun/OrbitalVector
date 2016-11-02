a9b85599b908c184f88740b577fc277a7bcc6e38
Si Li <omikun@gmail.com>
2016-11-02 08:53:24 -0700

    Added selector icon to porkchop plot
    bug: selector disappears in some portions of the plot (zdepth farther
    away than plot), but when looked from the side it's closer than plot.

---
+++ Assets/InteractablePlot.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+public class InteractablePlot : VRTK_InteractableObject {
+    GameObject selector;
+    public override void StartUsing(GameObject currentUsingObject)
+    {
+        base.StartUsing(currentUsingObject);
+
+        Ray ray = new Ray(currentUsingObject.transform.position, currentUsingObject.transform.forward);
+        RaycastHit hit;
+        if (GetComponent<Collider>().Raycast(ray, out hit, 100))
+        {
+            //SetSelectorPosition(hit.point);
+            selector.transform.position = hit.point + new Vector3(0,0,-.01f);
+        }
+        Debug.Log("Using plot!");
+    }
+    
+    void SetSelectorPosition(Vector3 point)
+	{
+        bool fixX = false;
+        bool fixY = false;
+        var temp = selector.transform.localPosition;
+        selector.transform.position = point;
+		selector.transform.localPosition = new Vector3(fixX ? selector.transform.localPosition.x : point.x, fixY ? selector.transform.localPosition.y : point.y, selector.transform.localPosition.z);
+	}
+    public override void StopUsing(GameObject previousUsingObject)
+    {
+        base.StopUsing(previousUsingObject);
+    }
+	// Use this for initialization
+	void Start () {
+        base.Start();
+        selector = GameObject.Find("selector");
+        if (selector == null)
+            Debug.Log("cann't find selector in plot");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
