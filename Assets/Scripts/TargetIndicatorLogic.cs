75f185d85ec592ccac9cf3340650ed3dc865bd12
Si Li <omikun@gmail.com>
2017-09-11 22:59:07 -0700

    separated state machine into partial class file, replaces previous speghetti code, cleaned up

--- Assets/Scripts/TargetIndicatorLogic.cs
+++ Assets/Scripts/TargetIndicatorLogic.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
-public class TargetIndicatorLogic : MonoBehaviour {
+public partial class TargetIndicatorLogic : MonoBehaviour {
     public GameObject camera;
     public GameObject target; //this needs to be set on each new object
     public GameObject center; //center of player (origin)
@@ -14,21 +14,21 @@ public class TargetIndicatorLogic : MonoBehaviour {
     public GameObject arrowIcon;
     Plane[] planes;
     GameObject canvas;
-    SpriteRenderer sr;
+    SpriteRenderer squareIconSR;
 
     public GameObject selectedTargetIcon;
-    SpriteRenderer stsr;
+    SpriteRenderer selectedIconSR;
 
     public GameObject lockedIcon;
-    SpriteRenderer lsr;
+    SpriteRenderer lockedIconSR;
 
     //icon for sphere UI
     public GameObject sphericalTargetIcon; //sti
-    SpriteRenderer ssr;
+    SpriteRenderer sphericalIconSR;
     //base if spherical target icon
     public GameObject stiBase;
-    SpriteRenderer sbsr;
-    LineRenderer stiLR; //line extending from sti to stiBase
+    SpriteRenderer sphericalIconBaseSR;
+    LineRenderer sphericalIconLR; //line extending from sti to stiBase
 
     public AudioClip lockedAudio;
     public AudioClip unlockedAudio; //falling out of fov
@@ -36,109 +36,25 @@ public class TargetIndicatorLogic : MonoBehaviour {
     public AudioClip selectedAudio;
     public AudioClip inFOVAudio;
     AudioSource audio;
-	void Start () {
+    void Start () {
         audio = GetComponent<AudioSource>();
         canvas = topBound.transform.parent.gameObject;
-        sr = squareIcon.GetComponent<SpriteRenderer>();
-        lsr = lockedIcon.GetComponent<SpriteRenderer>();
-        stsr = selectedTargetIcon.GetComponent<SpriteRenderer>();
-        stiLR = sphericalTargetIcon.GetComponent<LineRenderer>();
-        ssr = sphericalTargetIcon.GetComponent<SpriteRenderer>();
-        sbsr = stiBase.GetComponent<SpriteRenderer>();
-        NextState = UnselectedState;
-	}
-
-    /// <summary>
-    /// unselected -> selected
-    /// selected -> locking
-    /// locking -> selected OR unselected OR locked
-    /// locked -> selected OR unselected
-    /// </summary>
-    //state delegates in FSM
-    Action NextState = null;
-    void InitialState()
-    {
-
+        squareIconSR = squareIcon.GetComponent<SpriteRenderer>();
+        lockedIconSR = lockedIcon.GetComponent<SpriteRenderer>();
+        selectedIconSR = selectedTargetIcon.GetComponent<SpriteRenderer>();
+        sphericalIconLR = sphericalTargetIcon.GetComponent<LineRenderer>();
+        sphericalIconSR = sphericalTargetIcon.GetComponent<SpriteRenderer>();
+        sphericalIconBaseSR = stiBase.GetComponent<SpriteRenderer>();
+        NextState = InitialState;
     }
-    void GoneState()
-    {
-        NextState = EndState;
-    }
-    void EndState()
-    {
-        return;
-    }
-    void UnselectedState()
-    {
-        if (target == UXStateManager.GetTarget())
-        {
-            NextState = SelectedState;
-            Debug.Log("state: selected");
-        } else
-        {
-        }
-    }
-    float BeginLocking = 0;
-    void SelectedState()
-    {
-        if (IsInFOV(target))
-        {
-            if (target != UXStateManager.GetTarget())
-            {
-                NextState = UnselectedState;
-                Debug.Log("state: UNselected");
-            }
-            else
-            {
-                //locking
-                BeginLocking = Time.time;
-                NextState = LockingState;
-                Debug.Log(name + "state: locking!");
-            }
-        } else
-        {
-            //regular selected state animation
-        }
-    }
-    void LockingState()
-    {
-        if (Time.time > BeginLocking + 2f)
-        {
-            NextState = LockedState;
-            Debug.Log(name + "state: locked");
-        }
-        if (!IsInFOV(target))
-        {
-            NextState = SelectedState;
-            Debug.Log("state: selected");
-        }
-        if (target != UXStateManager.GetTarget()) {
-            NextState = UnselectedState;
-            Debug.Log("state: UNselected");
-        }
-    }
-    void LockedState()
-    {
-        if (!IsInFOV(target))
-        {
-            NextState = SelectedState;
-            Debug.Log("state: selected");
-        }
-        if (target != UXStateManager.GetTarget()) {
-            NextState = UnselectedState;
-            Debug.Log("state: UNselected");
-        }      
-    }
-
-    /// //////////////////////////////////////////////////////////////////////
 
     void SetSphericalTargetIcon()
     {
         var basePos = target.transform.position.normalized * 2;
         var pos = basePos * 1.1f;
         sphericalTargetIcon.transform.position = pos;
-        stiLR.SetPosition(0, pos);
-        stiLR.SetPosition(1, basePos);
+        sphericalIconLR.SetPosition(0, pos);
+        sphericalIconLR.SetPosition(1, basePos);
         stiBase.transform.position = basePos;
         stiBase.transform.forward = pos;
 
@@ -147,36 +63,14 @@ public class TargetIndicatorLogic : MonoBehaviour {
     float oldAngle = 0;
     //know when a target is first locked
     float BeginTargetLockTime = 0;
-    float TimeToLock = 2f;
-    bool TargetLocked = false;
-    bool BeginTargetLock = false;
+    public float TimeToLock = 2f;
 
-
-	void FixedUpdate ()
+    void FixedUpdate ()
     {
-        NextState();
-        //target = UXStateManager.GetTarget();
-        if (target == null)
-        {
-            sr.enabled = false;
-            lsr.enabled = false;
-            stsr.enabled = false;
-            ssr.enabled = false;
-            stiLR.enabled = false;
-            sbsr.enabled = false;
-            return;
-        }
-        ssr.enabled = true;
-        sbsr.enabled = true;
-        stiLR.enabled = true;
-        stsr.enabled = (target == UXStateManager.GetTarget());
-        lsr.enabled = (target == UXStateManager.GetTarget());
-
-        LockAnimation();
-
-        sr.enabled = true;
         SetSphericalTargetIcon();
         SetSquareAndArrowIcon();
+
+        FSM();
     }
 
     private void SetSquareAndArrowIcon()
@@ -186,7 +80,7 @@ public class TargetIndicatorLogic : MonoBehaviour {
         Vector3 projectedPoint = target.transform.position - d;
         if (IsInViewFrustrum(target))
         {
-            sr.enabled = true;
+            squareIconSR.enabled = true;
             arrowIcon.SetActive(false);
             var distToCamera = canvas.transform.position - camera.transform.position;
             var targetIconPos = distToCamera.magnitude * (target.transform.position - camera.transform.position).normalized;
@@ -194,96 +88,18 @@ public class TargetIndicatorLogic : MonoBehaviour {
         }
         else
         {
-            sr.enabled = false;
+            squareIconSR.enabled = false;
             arrowIcon.SetActive(true);
             FindInterSection(projectedPoint);
-            if (false) //TODO simpler method, but can't get it to work atm
-            {
-                //arrowIcon.transform.up = (projectedPoint).normalized;
-                //arrowIcon.transform.up = (target.transform.position - transform.position).normalized;
-                //arrowIcon.transform.forward = camera.transform.position;
-                //var angle = Vector3.Angle(transform.up, projectedPoint);
-                arrowIcon.transform.rotation.SetLookRotation(camera.transform.forward, projectedPoint - transform.position);
-                //forward.transform.localPosition = camera.transform.forward.normalized * 18;
-                //up.transform.localPosition = (projectedPoint - transform.position).normalized * 18;
-            }
-            else
-            {
-                var angle = AngleSigned(transform.up, projectedPoint, transform.forward);
-                var diffAngle = angle - oldAngle;
-                oldAngle = angle;
-                var localRot = arrowIcon.transform.localRotation;
-                arrowIcon.transform.localRotation *= Quaternion.Euler(0, 0, diffAngle);
 
-                //arrowIcon.transform.Rotate(camera.transform.forward.normalized, diffAngle);
-
-                //arrowIcon.transform.rotation.SetLookRotation(camera.transform.position, projectedPoint);
-                //arrowIcon.transform.rotation.SetFromToRotation(Vector3.up, projectedPoint.normalized);
-                //Logdump.Log("angle: " + diffAngle);
-            }
+            var angle = AngleSigned(transform.up, projectedPoint, transform.forward);
+            var diffAngle = angle - oldAngle;
+            oldAngle = angle;
+            var localRot = arrowIcon.transform.localRotation;
+            arrowIcon.transform.localRotation *= Quaternion.Euler(0, 0, diffAngle);
         }
     }
 
-    //only perform lock if in fov and target is selected (stsr.enabled)
-    private void LockAnimation()
-    {
-        if (lsr.enabled && IsInFOV(target))
-        {
-            if (stsr.enabled)
-            {
-                if (TargetLocked == true)
-                {
-                    var startingPos = camera.transform.position;
-                    var endingPos = startingPos + startingPos.magnitude / 2 * (target.transform.position - camera.transform.position).normalized;
-                    lockedIcon.transform.position = endingPos;
-                }
-                else if (BeginTargetLock == false)
-                {
-                    BeginTargetLock = true;
-                    BeginTargetLockTime = Time.time;
-                    lsr.enabled = true;
-                }
-                else
-                {
-                    //move targetLockIcon from center screen to targetIcon position
-                    var startingPos = camera.transform.position;
-                    var endingPos = startingPos + startingPos.magnitude / 2 * (target.transform.position - camera.transform.position).normalized;
-                    var t = Mathf.Min(1, (Time.time - BeginTargetLockTime) / TimeToLock);
-                    lockedIcon.transform.position = Vector3.Lerp(startingPos, endingPos, t);
-                    if (t == 1)
-                    {
-                        TargetLocked = true;
-                        lsr.enabled = TargetLocked;
-                        audio.clip = lockedAudio;
-                        audio.loop = false;
-                        audio.Play();
-                    }
-                    else
-                    {
-                        if (audio.clip != lockingAudio)
-                        {
-                            audio.loop = true;
-                            audio.clip = lockingAudio;
-                            audio.Play();
-                        }
-                    }
-                }
-            }
-        }
-        else
-        {
-            if (audio.clip == lockingAudio)
-            {
-                audio.clip = unlockedAudio;
-                audio.loop = false;
-                audio.Play();
-            }
-            TargetLocked = false;
-            BeginTargetLock = false;
-            stsr.enabled = false;
-            lsr.enabled = false;
-        }
-    }
 
     bool IsInFOV(GameObject target)
     {
@@ -295,12 +111,11 @@ public class TargetIndicatorLogic : MonoBehaviour {
     float AngleSigned(Vector3 v1, Vector3 v2, Vector3 n)
     {
         return Mathf.Atan2(
-            Vector3.Dot(n, Vector3.Cross(v1, v2)),
-            Vector3.Dot(v1, v2)) * Mathf.Rad2Deg;
+                Vector3.Dot(n, Vector3.Cross(v1, v2)),
+                Vector3.Dot(v1, v2)) * Mathf.Rad2Deg;
     }
     bool IsInViewFrustrum(GameObject target)
     {
-        bool ret = false;
         planes = GeometryUtility.CalculateFrustumPlanes(camera.GetComponent<Camera>());
         Collider objCol = target.GetComponent<Collider>();
         return GeometryUtility.TestPlanesAABB(planes, objCol.bounds);
---
+++ Assets/Scripts/TargetIndicatorStates.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public partial class TargetIndicatorLogic
+{
+    //state delegates in FSM
+    /// <summary>
+    /// unselected -> selected
+    /// selected -> locking
+    /// locking -> unlocked OR locked
+    /// locked -> unlocked
+    /// unlocked -> selected
+    /// any state -> unselected
+    /// any state -> gone -> end
+    /// start up/end -> InitialState
+    /// </summary>
+    Action NextState = null;
+    void InitialState() //assumes prev target was destroyed and is coming from EndState
+    {
+        //squareIconSR.enabled = true; this is determined by squareIcon function
+        sphericalIconSR.enabled = true;
+        sphericalIconBaseSR.enabled = true;
+        sphericalIconLR.enabled = true;
+
+        var isSelected = (target == UXStateManager.GetTarget());
+        if (isSelected)
+            NextState = SelectedState;
+        else
+            NextState = UnselectedState;
+
+        selectedIconSR.enabled = isSelected;
+        lockedIconSR.enabled = isSelected;
+    }
+    void GoneState() //target is destroyed; clean up icons
+    {
+        NextState = EndState;
+        squareIconSR.enabled = false;
+        lockedIconSR.enabled = false;
+        selectedIconSR.enabled = false;
+        sphericalIconSR.enabled = false;
+        sphericalIconLR.enabled = false;
+        sphericalIconBaseSR.enabled = false;
+    }
+    void EndState()
+    {
+        return;
+    }
+
+    void FSM()
+    {
+        if (NextState == EndState) return;
+        if (target == null) NextState = GoneState;
+        NextState();
+    }
+
+    void UnselectedState()
+    { 
+        if (target == UXStateManager.GetTarget())
+        {
+            NextState = SelectedState;
+            selectedIconSR.enabled = true;
+            Debug.Log("state: selected");
+        } else
+        {
+            //steady state for unselected
+        }
+    }
+    void SelectedState()
+    {
+        if (IsInFOV(target)) //transition to lockingstate
+        {
+            NextState = LockingState;
+            BeginTargetLockTime = Time.time;
+            lockedIconSR.enabled = true;
+
+            audio.loop = true;
+            audio.clip = lockingAudio;
+            audio.Play();
+
+            Debug.Log(name + "state: locking!");
+        } else
+        {
+            //regular selected state animation
+        }
+    }
+
+    void LockingState()
+    {
+        if (TryUnlockState()) { }
+        else if (Time.time > BeginTargetLockTime + 2f) //transition to LOCKED!
+        {
+            lockedIconSR.enabled = true;
+            audio.clip = lockedAudio;
+            audio.loop = false;
+            audio.Play();
+
+            NextState = LockedState;
+            Debug.Log(name + "state: locked");
+        } else {
+            //move targetLockIcon from center screen to targetIcon position
+            var startingPos = camera.transform.position;
+            var endingPos = startingPos + startingPos.magnitude / 2 * (target.transform.position - camera.transform.position).normalized;
+            var t = Mathf.Min(1, (Time.time - BeginTargetLockTime) / TimeToLock);
+            lockedIcon.transform.position = Vector3.Lerp(startingPos, endingPos, t);
+        }
+    }
+
+    void LockedState()
+    {
+        if (TryUnlockState()) { }
+        else {
+            var startingPos = camera.transform.position;
+            var endingPos = startingPos + startingPos.magnitude / 2 * (target.transform.position - camera.transform.position).normalized;
+            lockedIcon.transform.position = endingPos;
+        }
+    }
+
+    bool TryUnlockState()
+    {
+        if (!IsInFOV(target)) //kick out of LockingState
+        {
+            audio.clip = unlockedAudio;
+            audio.loop = false;
+            audio.Play();
+
+            lockedIconSR.enabled = false;
+            NextState = SelectedState; //could jump straight to unselected, but next update will do that if need be
+            Debug.Log("state: selected");
+            return true;
+        }
+        return false;
+    }
+}
